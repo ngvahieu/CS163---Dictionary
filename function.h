dc65c961a3e29d813a9d5b6614fb4876ebd647a0
ngvahieu <vanhieu230303@gmail.com>
2022-06-20 23:11:03 +0700

    update functions

--- function.h
+++ function.h
@@ -13,26 +13,26 @@ struct Node {
 };
 class Dictionary{
 public: 
-    vector<pair<string,Node*>> tab1; // tab1 stores emojis data sets.
-    vector<pair<string,Node*>> tab2; // tab2 stores slang words data sets
-	vector<pair<string,Node*>> tab3; // tab3 stores English to English data sets.
-	vector<pair<string,Node*>> tab4; // tab4 stores English to Vietnamese data sets
-	vector<pair<string,Node*>> tab5; // tab5 stores VietNamese to English data sets
+    Node* tab1; // tab1 stores emojis data sets.
+    Node* tab2; // tab2 stores slang words data sets
+	Node* tab3; // tab3 stores English to English data sets.
+	Node* tab4; // tab4 stores English to Vietnamese data sets
+	Node* tab5; // tab5 stores VietNamese to English data sets
 
-    void getFileName(vector<pair<string,Node*>>& tab); // order: 1->5, get all file names in the same data set.
-    void getAllWords(vector<pair<string,Node*>>& tab); // get all words in a specific file.
-    string searchForKeyword(string keyword,vector<pair<string,Node*>>& tab);
-    string searchForDef(string def,vector<pair<string,Node*>>& tab);
+    void getAllWords(Node*& tab); // get all words in a specific file.
+    string searchForKeyword(string keyword,Node*& tab);
+    string searchForDef(string def,Node*& tab);
     void viewHistoryWords();
-    void addNewWord(string word,string def,vector<pair<string,Node*>>& tab);
-    void editWord(string word,vector<pair<string,Node*>>& tab);
-    void removeWord(string word,vector<pair<string,Node*>>& tab);
-    void resetDictionary(vector<pair<string,Node*>>& tab);
+    void addNewWord(string word,string def,Node*& tab);
+    void editWord(string word,Node*& tab);
+    void removeWord(string word,Node*& tab);
+    void resetDictionary(Node*& tab);
     void removeFavWord(string word);
     void addFavWord(string word);
-    void viewRanWord(vector<pair<string,Node*>>& tab);
-    void guessDef(vector<pair<string,Node*>>& tab);
-    void guessWord(vector<pair<string,Node*>>& tab);
+    void viewRanWord(Node*& tab);
+    void guessDef(Node*& tab);
+    void guessWord(Node*& tab);
 };
 int charToIndex(char a); // change a character to equivalent index in TrieNode
 vector<char> splitWord(string word); // split a word into characters.
+void saveTreeToFile(Node*& tab); // save Trie tree to txt file
