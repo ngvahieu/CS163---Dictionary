d401bd5085f271b1057d7f5d0f469924924278ca
ngvahieu <vanhieu230303@gmail.com>
2022-06-29 22:43:24 +0700

    put words to Trie tree

--- function.cpp
+++ function.cpp
@@ -1,6 +1,6 @@
 #include"function.h"
 // supporting functions
-int charToIndex(char a){
+int charToIndex(char a) {
     return a - 33;
 }
 vector<char> splitWord(string word) {
@@ -10,117 +10,163 @@ vector<char> splitWord(string word) {
     while (ss >> a) result.push_back(a);
     return result;
 }
-// main functions
 
 vector<Word> getWordsFromFile(int choice) {
     vector<Word> dict;
-   _setmode(fileno(stdin), _O_U16TEXT);
-   _setmode(fileno(stdout), _O_U16TEXT);
-   locale loc(locale(), new codecvt_utf8<wchar_t>);
-    
+
     switch (choice) {
-        case 1: { // emojis
-            ifstream fin; fin.open("/dataSets/emotional.txt", ios::in);
-            char check;
-            string firstTaken, secondTaken;
-            Word temp;
-            while(!fin.eof()) {
-                getline(fin, firstTaken, ':');
-                fin.get(check);
-                if(check != ' ') {
-                    if(check == ':') {
-                        getline(fin, secondTaken, '\n');
-                        temp.key = firstTaken + check;
-                        temp.def = secondTaken;
-                    }
-                    else {
-                        getline(fin, secondTaken, ':');
-                        temp.key = firstTaken + ':' + check + secondTaken;
-                        getline(fin, secondTaken, '\n');
-                        temp.def = secondTaken;
-                    }
-                }
-                else { // ...:(space)...
+    case 1: { // emojis
+        ifstream fin; fin.open("dataSets/emotional.txt", ios::in);
+        char check;
+        string firstTaken, secondTaken;
+        Word temp;
+        while (!fin.eof()) {
+            getline(fin, firstTaken, ':');
+            fin.get(check);
+            if (check != ' ') {
+                if (check == ':') {
                     getline(fin, secondTaken, '\n');
-                    temp.key = firstTaken;
-                    temp.def = check + secondTaken;
+                    temp.key = firstTaken + check;
+                    temp.def = secondTaken;
                 }
-                temp.def.erase(temp.def.begin());
-                dict.push_back(temp);
-            }
-            fin.close();
-        }
-            break;
-        case 2: { // slang
-            ifstream fin; fin.open("/dataSets/slang.txt", ios::in);
-            Word temp;
-            char check;
-            string firstTaken, secondTaken, s;
-            wstring w;
-            while(!fin.eof()) {
-                getline(fin, firstTaken, ':');
-                fin.get(check);
-                if(check != ' ') {
-                    if(check == ':') {
-                        getline(fin, secondTaken, '\n');
-                        temp.key = firstTaken + check; temp.def = secondTaken;
-                    }
-                    else {
-                        getline(fin, secondTaken, ':');
-                        temp.key = firstTaken + ':' + check + secondTaken;
-                        getline(fin, secondTaken, '\n');
-                        temp.def = secondTaken;
-                    }
-                }
-                else { // ...:(space)...
+                else {
+                    getline(fin, secondTaken, ':');
+                    temp.key = firstTaken + ':' + check + secondTaken;
                     getline(fin, secondTaken, '\n');
-                    temp.key = firstTaken;
-                    temp.def = check + secondTaken;
+                    temp.def = secondTaken;
                 }
-                temp.def.erase(temp.def.begin());
-                dict.push_back(temp);
             }
-            fin.close();
-        }
-            break;
-        case 3: { // Eng to Eng
-            ifstream fin; fin.open("/dataSets/eng-eng.txt", ios::in);
-            Word temp;
-            string s;
-            while(!fin.eof()) {
-                getline(fin, temp.key, ':');
-                fin.ignore(1);
-                getline(fin, temp.def, '\n');
-                dict.push_back(temp);
+            else { // ...:(space)...
+                getline(fin, secondTaken, '\n');
+                temp.key = firstTaken;
+                temp.def = check + secondTaken;
             }
-            fin.close();
+            temp.def.erase(temp.def.begin());
+            dict.push_back(temp);
         }
-            break;
-        case 4: { // Eng to Viet
-            ifstream fin; fin.open("/dataSets/eng-viet.txt", ios::in);
-            Word temp;
-            string s;
-            while(!fin.eof()) {
-                getline(fin, temp.key, ':');
-                fin.ignore(1);
-                getline(fin, temp.def, '\n');
-                dict.push_back(temp);
+        fin.close();
+    }
+          break;
+    case 2: { // slang
+        ifstream fin; fin.open("dataSets/slang.txt", ios::in);
+        Word temp;
+        char check;
+        string firstTaken, secondTaken, s;
+        wstring w;
+        while (!fin.eof()) {
+            getline(fin, firstTaken, ':');
+            fin.get(check);
+            if (check != ' ') {
+                if (check == ':') {
+                    getline(fin, secondTaken, '\n');
+                    temp.key = firstTaken + check; temp.def = secondTaken;
+                }
+                else {
+                    getline(fin, secondTaken, ':');
+                    temp.key = firstTaken + ':' + check + secondTaken;
+                    getline(fin, secondTaken, '\n');
+                    temp.def = secondTaken;
+                }
             }
-            fin.close();
-        }
-            break;
-        case 5: { // Viet to Eng
-            ifstream fin; fin.open("/dataSets/viet-eng.txt", ios::in);
-            Word temp;
-            while(!fin.eof()) {
-                getline(fin, temp.key, ':');
-                fin.ignore(1);
-                getline(fin, temp.def, '\n');
-                dict.push_back(temp);
+            else { // ...:(space)...
+                getline(fin, secondTaken, '\n');
+                temp.key = firstTaken;
+                temp.def = check + secondTaken;
             }
-            fin.close();
+            temp.def.erase(temp.def.begin());
+            dict.push_back(temp);
         }
-            break;
+        fin.close();
+    }
+          break;
+    case 3: { // Eng to Eng
+        ifstream fin; fin.open("dataSets/eng-eng.txt", ios::in);
+        Word temp;
+        string s;
+        while (!fin.eof()) {
+            getline(fin, temp.key, ':');
+            fin.ignore(1);
+            getline(fin, temp.def, '"');
+            string pass;
+            getline(fin, pass, '\n');
+            dict.push_back(temp);
+        }
+        fin.close();
+    }
+          break;
+    case 4: { // Eng to Viet
+        ifstream fin; fin.open("dataSets/eng-viet.txt", ios::in);
+        Word temp;
+        string s;
+        while (!fin.eof()) {
+            getline(fin, temp.key, ':');
+            fin.ignore(1);
+            getline(fin, temp.def, '\n');
+            dict.push_back(temp);
+        }
+        fin.close();
+    }
+          break;
+    case 5: { // Viet to Eng
+        ifstream fin; fin.open("dataSets/viet-eng.txt", ios::in);
+        Word temp;
+        while (!fin.eof()) {
+            getline(fin, temp.key, ':');
+            fin.ignore(1);
+            getline(fin, temp.def, '\n');
+            dict.push_back(temp);
+        }
+        fin.close();
+    }
+          break;
     }
     return dict;
+}
+void deleteTree(Node*& tree) {
+    if (!tree) return;
+    for (int i = 0; i < 94; ++i) {
+        deleteTree(tree->child[i]);
+    }
+    delete tree;
+}
+// main functions
+void inputWordToTree(Node*& root, Word word)
+{
+    if (!root) root = new Node();
+    Node* pCrawl = root;
+
+    for (int i = 0; i < word.key.length(); i++) {
+        int index = charToIndex(word.key[i]);
+        if (!pCrawl->child[index])
+            pCrawl->child[index] = new Node();
+
+        if (i == 0) {
+            char first = word.def[0];
+            for (int j = 0; j < word.def.length(); j++) {
+                if (word.def[j] != ' ') {
+                    first = word.def[j];
+                    break;
+                }
+            }
+            vector<string> Def;
+            Def.push_back(word.def);
+            int x = charToIndex(first);
+            if (!pCrawl->child[x]) pCrawl->child[x] = new Node();
+            pCrawl->child[x]->defList.push_back({ Def,word.key });
+        }
+        pCrawl = pCrawl->child[index];
+    }
+
+    // mark last node as leaf
+    pCrawl->isLastChar = true;
+    vector<string> Def;
+    Def.push_back(word.def);
+    pCrawl->def.push_back(Def);
+}
+
+void Dictionary :: getAllWordsToTree(Node*& tree, int choice) {
+    vector<Word> dic = getWordsFromFile(choice);
+    for (int i = 0; i < dic.size(); i++) {
+        inputWordToTree(tree, dic[i]);
+    }
 }
--- function.h
+++ function.h
@@ -13,42 +13,49 @@ using namespace std;
 struct Node {
     Node* child[94]; // from 33->126 in ASCII
     bool isLastChar;
-	bool add;    
+    bool add;
     bool remove;
-    vector<pair<string,vector<string>>> def; 
+    vector<vector<string>> def;
     // string saves a key 
     // vector<string> saves definitions.
     // just push if a node is a last character of a specific word.
-    vector<pair<vector<string>,string>> defList; 
+
+    vector<pair<vector<string>, string>> defList;
     // only save to first character Node for searching definition.
+    Node() {
+        isLastChar = false; add = false; remove = false;
+        for (int i = 0; i < 94; i++) child[i] = nullptr;
+    }
 };
-struct Word{
+struct Word {
     string key;
     string def;
 };
-class Dictionary{
-public: 
-    Node* tree1; // tree1 stores emojis data set.
+class Dictionary {
+public:
+    Node* tree1; // tree1 stores emotional data set.
     Node* tree2; // tree2 stores slang words data set
-	Node* tree3; // tree3 stores English to English data set.
-	Node* tree4; // tree4 stores English to Vietnamese data set.
-	Node* tree5; // tree5 stores VietNamese to English data set.
+    Node* tree3; // tree3 stores English to English data set.
+    Node* tree4; // tree4 stores English to Vietnamese data set.
+    Node* tree5; // tree5 stores VietNamese to English data set.
 
-    void getAllWordsToTree(Node*& tree,int choice); // get all words in a specific file.
-    string searchForKeyword(string keyword,Node*& tree,int choice);
-    string searchForDef(string def,Node*& tree,int choice);
+    void getAllWordsToTree(Node*& tree, int choice); // get all words in a specific file.
+    string searchForKeyword(string keyword, Node*& tree, int choice);
+    string searchForDef(string def, Node*& tree, int choice);
     void viewHistoryWords(int choice);
-    void addNewWord(string word,string def,Node*& tree,int choice);
-    void editWord(string word,Node*& tree,int choice);
-    void removeWord(string word,Node*& tree,int choice);
-    void resetDictionary(Node*& tree,int choice);
-    void removeFavWord(string word,int choice);
-    void addFavWord(string word,int choice);
-    void viewRanWord(Node*& tree,int choice);
-    void guessDef(Node*& tree,int choice);
-    void guessWord(Node*& tree,int choice);
+    void addNewWord(string word, string def, Node*& tree, int choice);
+    void editWord(string word, Node*& tree, int choice);
+    void removeWord(string word, Node*& tree, int choice);
+    void resetDictionary(Node*& tree, int choice);
+    void removeFavWord(string word, int choice);
+    void addFavWord(string word, int choice);
+    void viewRanWord(Node*& tree, int choice);
+    void guessDef(Node*& tree, int choice);
+    void guessWord(Node*& tree, int choice);
 };
 int charToIndex(char a); // change a character to equivalent index in TrieNode.
 vector<char> splitWord(string word); // split a word into characters.
 vector<Word> getWordsFromFile(int choice);
-void saveTreeToFile(Node*& tree,int choice); // save Trie tree to text file.
+void inputWordToTree(Node*& root, Word word); // put a word to tree
+void saveTreeToFile(Node*& tree, int choice); // save Trie tree to text file.
+void deleteTree(Node*& tree); // delete a Trie tree 
